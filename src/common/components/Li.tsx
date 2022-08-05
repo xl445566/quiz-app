83eb6d992cb711521ce058cbac02af39be56eb68
seodongsu <xl445566@naver.com>
2022-08-06 03:41:55 +0900

    refactor: Improve common components

---
+++ src/common/components/Li.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Li = styled.li`
+  padding: 10px;
+`;
+
+export default Li;
---
+++ src/common/components/Title.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  padding: 20px;
+  color: var(--point-color);
+`;
+
+export default Title;
