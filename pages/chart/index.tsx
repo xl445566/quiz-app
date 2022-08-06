5dd2faeb7e05d9bff9f5525673d617e24d2af6f3
seodongsu <xl445566@naver.com>
2022-08-07 00:46:48 +0900

    refactor: Change style

--- pages/chart/index.tsx
+++ pages/chart/index.tsx
@@ -1,11 +1,80 @@
 import type { NextPage } from "next";
 
-import styled from "styled-components";
+import { useRouter } from "next/router";
+import { useStore } from "../../src/lib/store";
+import styled, { ContentProps } from "styled-components";
+
+import printTime from "../../src/common/utils/printTime";
+
+import DonutChart from "../../src/features/chart/DonutChart";
+import Section from "../../src/common/components/Section";
+import Title from "../../src/common/components/Title";
+import Strong from "../../src/common/components/Strong";
+import BottonContainer from "../../src/common/components/ButtonContainer";
+import Button from "../../src/common/components/Button";
+
+const ContentContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.h1<ContentProps>`
+  padding: 20px;
+  color: ${(props) => {
+    if (props.value) {
+      return "var(--point-color)";
+    } else {
+      return "var(--red-color)";
+    }
+  }};
+  font-size: 1.5rem;
+`;
 
 const Chart: NextPage = () => {
-  // 정답, 오답 개수
-  // 소요 시간
-  return <h1>차트 화면</h1>;
+  const { time, correctQuestions, incorrectQuestions, resetQuizData } =
+    useStore();
+  const router = useRouter();
+
+  const handleQuizRetry = () => {
+    router.push("/problem");
+  };
+
+  const handleHomePage = () => {
+    resetQuizData();
+    router.push("/");
+  };
+
+  const handleNotePage = () => {
+    router.push("/note");
+  };
+
+  return (
+    <>
+      <Section>
+        <Title>
+          <Strong>결과</Strong>
+        </Title>
+
+        <DonutChart
+          time={printTime(time)}
+          correctQuestions={correctQuestions}
+          incorrectQuestions={incorrectQuestions}
+        />
+
+        <ContentContainer>
+          <Content value={true}>정답: {correctQuestions.length}</Content>
+          <Content value={false}>오답: {incorrectQuestions.length}</Content>
+        </ContentContainer>
+
+        <BottonContainer>
+          <Button label="다시 풀기" onClick={handleQuizRetry} />
+          <Button label="처음 으로" onClick={handleHomePage} />
+          <Button label="오답 노트" onClick={handleNotePage} />
+        </BottonContainer>
+      </Section>
+    </>
+  );
 };
 
 export default Chart;
--- src/common/components/Title.tsx
+++ src/common/components/Title.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Title = styled.h1`
   padding: 20px;
   color: var(--point-color);
+  font-size: 2rem;
 `;
 
 export default Title;
