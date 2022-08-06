9122e7d64909761c03ac2a5ace6aead87c99d3e5
seodongsu <xl445566@naver.com>
2022-08-06 19:58:04 +0900

    feat: Add chart page

---
+++ pages/chart/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+import styled from "styled-components";
+
+const Chart: NextPage = () => {
+  // 정답, 오답 개수
+  // 소요 시간
+  return <h1>차트 화면</h1>;
+};
+
+export default Chart;
