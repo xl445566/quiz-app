3cb7c64321fc90b52f46e45c01e0b89065f5afb9
seodongsu <xl445566@naver.com>
2022-08-10 11:47:09 +0900

    refactor: Move _app -> _document meta, link tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,7 +40,34 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="Random quiz web app" />
+          <meta
+            name="description"
+            content="You can check the results of the quiz on the chart
+          You can create incorrect answer notes."
+          />
+          <meta
+            name="keywords"
+            content="Quiz, App, Problem, Solve, Random, Note"
+          ></meta>
+          <meta property="og:title" content="Random Quiz App" />
+          <meta property="og:url" content="https://quizquizapp.netlify.app" />
+          <meta property="og:type" content="Web-App" />
+          <meta
+            property="og:image"
+            content="https://user-images.githubusercontent.com/78071591/183364643-d2a50dab-07da-457e-9566-8ec5f525b43c.png"
+          />
+          <meta property="og:site_name" content="Quiz-App" />
+          <meta
+            property="og:description"
+            content="You can check the results of the quiz on the chart
+          You can create incorrect answer notes."
+          />
+          <link rel="icon" href="/logo.ico" />
+        </Head>
         <body>
           <Main />
           <div id="modal" />
