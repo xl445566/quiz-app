2abb5cb4c64104e7bc7c9c7827740904563b8ffb
seodongsu <xl445566@naver.com>
2022-08-10 11:44:53 +0900

    refactor: Change _document style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,52 @@
 import type { NextPage } from "next";
 
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-const Document: NextPage = () => {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <div id="modal" />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-export default Document;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <div id="modal" />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
