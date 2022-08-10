bdbe696ac6023dca61b2917abce65841697edf4d
seodongsu <xl445566@naver.com>
2022-08-10 14:35:47 +0900

    refactor: Change loading text style

--- src/common/components/Loading.tsx
+++ src/common/components/Loading.tsx
@@ -19,14 +19,15 @@ const Wrapper = styled.div`
   display: flex;
   align-items: flex-end;
   justify-content: center;
+  color: var(--pointer-color);
 `;
 
-export const Dot = styled.div<DotProps>`
-  background-color: black;
-  border-radius: 50%;
+const Dot = styled.div<DotProps>`
   width: 5px;
   height: 5px;
   margin: 0 2.5px;
+  border-radius: 50%;
+  background-color: var(--point-color);
 
   animation: ${BounceAnimation} 0.5s linear infinite;
   animation-delay: ${(props) => props.delay};
@@ -35,7 +36,7 @@ export const Dot = styled.div<DotProps>`
 const Loading: NextPage = () => {
   return (
     <Wrapper>
-      <h4>불러오는 중</h4>
+      <h5>불러오는 중</h5>
 
       <Dot delay="0s" />
       <Dot delay="0.1s" />
